bf153c20caa170142c66db6bfedd5476ed757171
alexdmacon <alexdmacon@gmail.com>
2022-01-22 11:42:58 -0600

    fixed react router routes, added run scripts

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
 
 {/* should contain nav bar and/or title/logo and should render on every page
  */}
+ <h1>HI HERE IS THE HEADER</h1>
 
   </div>;
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return <div>
-
+    <h1>Hi here is the home page</h1>
 {/* Landing page, splashy and attention-grabbing */}
 
   </div>;
