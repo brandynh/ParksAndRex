79c0b42763038a166fc8f22f7c1d5888aea9548c
alexdmacon <alexdmacon@gmail.com>
2022-01-22 23:37:57 -0600

    navbar collapses with click on centered logo in header, using placeholder logo for now

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import dinosaurImg from "../assets/images/j-park.jpeg"; 
 
 const Header = () => {
   return (
-      <Navbar className="justify-content-center">
-        <Navbar.Brand href="/">
-          <h1>PARKS AND REX LOGO</h1>
+    <Navbar expand="false" className="justify-content-center">
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <Navbar.Brand>
+        <img src={dinosaurImg} alt="dinosaurs"/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-        <Nav className="me-auto">
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/shop">Visit</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Navbar.Collapse>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto text-center">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/shop">Visit</Nav.Link>
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
         </Nav>
-      </Navbar>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
