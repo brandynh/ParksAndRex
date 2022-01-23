2a9b9ba6cc15476814ba50c389c8ab393268e271
alexdmacon <alexdmacon@gmail.com>
2022-01-22 22:35:19 -0600

    navbar links working, a lot of styling to be done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>
+  return (
+      <Navbar className="justify-content-center">
+        <Navbar.Brand href="/">
+          <h1>PARKS AND REX LOGO</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-{/* should contain nav bar and/or title/logo and should render on every page
- */}
- <h1>HI HERE IS THE HEADER</h1>
-
-  </div>;
+        <Nav className="me-auto">
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/shop">Visit</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Navbar.Collapse>
+        </Nav>
+      </Navbar>
+  );
 };
 
 export default Header;
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Contact = () => {
   return <div>
+    <h1>HI THIS IS THE CONTACT PAGE</h1>
 
 {/* Contact page, possibly with form submission if we want to get fancy with it. */}
 
