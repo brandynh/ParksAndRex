60977e59336fa26766abc0061ff0f387611caffd
alexdmacon <alexdmacon@gmail.com>
2022-01-24 19:42:58 -0600

    react component for tour packages written, with placeholders on shop page.

--- client/src/components/Package.js
+++ client/src/components/Package.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
-const Package = () => {
-  return <div>
-
-      {/* card component to easily make new items (packages, tours, etc.) 
-      on "Shop" page */}
-
-  </div>;
+const Package = ({imgLink, title, description, cost}) => {
+  return (
+    <div className="col-md-6 col-lg-4 mb-5">
+      <div className="mx-auto">
+        <div className="d-flex h-100 w-100">
+          <div>
+            <h2 align="center">{title}</h2>
+            <a target="_blank">
+            <img className="img-fluid project-img" src={imgLink}></img>
+            </a>
+            <p align="center">
+              {description}
+            <p>
+              <ul className="list-unstyled list-inline text-center">
+                <li>
+                  <em>Cost:</em>{cost}
+                </li>
+                  <li>
+                  <button>Buy This Tour Package</button>
+                  </li>
+              </ul>
+            </p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Package;
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,27 +1,42 @@
 import React from "react";
 import Package from "../components/Package";
+import dinoTestImg from "../assets/images/dino-desktop-2.jpeg"
+import dinoPark from "../assets/images/dino-desktop.jpeg"
+import dinoPark2 from "../assets/images/dinosaur-desktop.jpg"
 
 const Shop = () => {
   return (
-    <div>
-
+      <div className="container">
+        <div className="row justify-content-center">
       <h1> HI HERE IS THE SHOP PAGE</h1>
+      </div>
       {/* Feature basic tours/packages/tickets for purchase. Use Package component for each item. Start with 3, add more
     —and possibly other pages for gift shop, etc.—as we progress. */}
+    <div className="row justify-content-center">
       <Package
-
+          title="DINO PACK 1"
+          imgLink={dinoTestImg}
+          description="this package freaking rules man let me tell you all about it here dino ipsum bla bla bla "
+          cost="$55,000"
       /* package props in here */
       />
 
       <Package
-
+          title="DINO PACK 2"
+          imgLink={dinoPark}
+          description="this package freaking rules man let me tell you all about it here dino ipsum bla bla bla "
+          cost="$1 million"
       /* package props in here, etc. */
       />
 
       <Package
-
+          title="DINO PACK 3"
+          imgLink={dinoPark2}
+          description="this package freaking rules man let me tell you all about it here dino ipsum bla bla bla "
+          cost="$350,000"
       /* package props in here, etc. */
       />
+      </div>
     </div>
   );
 };
