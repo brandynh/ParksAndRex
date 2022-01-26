0862daef3db53bda60849420b22a54fe5ceddb1c
alexdmacon <alexdmacon@gmail.com>
2022-01-25 19:59:03 -0600

    removed border from logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
 
   return (
     <Navbar expand="false" className="justify-content-center navbar">
-      <Navbar.Toggle aria-controls="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="logo">
         <Navbar.Brand>
         <img className="img-fluid" src={dinosaurImg} alt="dinosaurs"/>
         </Navbar.Brand>
@@ -21,8 +21,8 @@ const Header = () => {
           <Nav.Link href="/shop">Visit</Nav.Link>
           <Nav.Link href="/checkout">Checkout</Nav.Link>
           <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/" onClick={() => Auth.logout()}>Logout</Nav.Link>
-          <Nav.Link href="/signup">Sign Up</Nav.Link>
+{/*           <Nav.Link href="/" onClick={() => Auth.logout()}>Logout</Nav.Link>
+ */}          <Nav.Link href="/signup">Sign Up</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
