3ccaeccd10e2b9365cebf94c6fe02d181093752b
alexdmacon <alexdmacon@gmail.com>
2022-01-25 21:43:02 -0600

    added copy on thanks page

--- client/src/pages/Thanks.js
+++ client/src/pages/Thanks.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Thanks = () => {
   return <div>
 
+    <h1>Congrats! You have booked your tickets to Parks and Rex.</h1>
 {/* Page that will render after user successfully completes order on Checkout page. Something that says "thanks, that worked," and provides link back to home page. */}
 
   </div>;
