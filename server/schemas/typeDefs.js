b406b7ac6d2972ed22353dc0fe32c64feecfcae7
Austin Pratt <hargis32@gmail.com>
2022-01-26 20:04:14 -0600

    updated resolvers and typedefs to query packages

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Category {
-    _id: ID
-    name: String
-  }
-
+  
   type Perk {
   _id: ID
   name: String
@@ -22,29 +18,12 @@ const typeDefs = gql`
   price: Float
   }
 
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
-
-  type Order {
-    _id: ID
-    purchaseDate: String
-    products: [Product]
-  }
-
   type User {
     _id: ID
     firstName: String
     lastName: String
     username: String
     email: String
-    orders: [Order]
   }
 
   type Checkout {
@@ -57,19 +36,13 @@ const typeDefs = gql`
   }
 
   type Query {
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
+    packages: [Package]
     user: User
-    order(_id: ID!): Order
     checkout(products: [ID]!): Checkout
   }
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, username: String!, email: String!, password: String!): Auth
-    addOrder(products: [ID]!): Order
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
-    updateProduct(_id: ID!, quantity: Int!): Product
     login(username: String!, password: String!): Auth
   }
 `;
