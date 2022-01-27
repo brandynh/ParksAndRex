3a88e242b6f09d7096edf040546fed92ff375b79
Brandyn <hufferbrandyn@gmail.com>
2022-01-26 20:48:46 -0600

    serviceWorker updated

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerReg from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+serviceWorkerReg.register();
 
 
--- server/config/seeds.js
+++ server/config/seeds.js
@@ -28,38 +28,39 @@ db.once('open', async () => {
 
    await Package.deleteMany();
 
-   const package = await Package.insertMany([
+   await Package.insertMany([
       {
          name: 'Velociraptor Package',
          description: 'For those wanting to experience the foundation of what makes Parks and Rex the premier safari adventure on Earth!',
-         perks: '',
+         perks: [],
          image: '',
-         price: '20,000',
+         price: '20000',
       },
       {
          name: 'T-Rex Package',
          description: 'If you really want to sink your teeth into everything that Parks and Rex has to offer, the T-Rex package has you covered.',
          image:'',
-         price: '50,000',  
+         price: '50000',  
       },
       {
          name: 'Brontosaurus Package',
          description: 'The ultimate experience. With the Brontosaurus package, nothing in Parks and Rex will be out of your reach',
          image: '',
-         price: '100,000'
+         price: '100000'
       }
    ]);
 
    await User.deleteMany();
 
    await User.create({
+      username: 'Badassasaurus',
       firstName: 'Joshua',
       lastName: 'Diehl',
       email: 'jdiehl@testmail.com',
       password: 'password11'
    });
 
-   console.log('users seeded');
+   console.log('Data seeded');
 
    process.exit();
 });
