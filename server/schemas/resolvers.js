3f63824b6b48cbd8a6d90a1293358a3cc835ed15
Zach Rojas <zachrojas94@gmail.com>
2022-01-26 21:13:39 -0600

    Added necessary files for Stripe implementation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -31,7 +31,7 @@ const resolvers = {
       const order = new Order({ products: args.products });
       const line_items = [];
 
-      const { products } = await order.populate('products').execPopulate();
+      const { packages } = await order.populate('packages').execPopulate();
 
       for (let i = 0; i < products.length; i++) {
         const product = await stripe.products.create({
