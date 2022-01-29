3e2e73f8824d4e46371a45cc0967c15eee51442a
NuminousBlue <jdiehl2236@gmail.com>
2022-01-28 21:06:25 -0600

    working on re-configuring nav links in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, } from "react-bootstrap";
 import dinosaurImg from "../assets/images/Dinologo1.png";
 import Auth from "../utils/auth";
 import '../header.css';
@@ -11,23 +11,39 @@ const Header = () => {
 
    return (
     <Navbar expand="false" className="navbar justify-content-center">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="logo" id="glowBox">
+      <Navbar.Toggle aria-controls="" className="logo" id="glowBox">
         <Navbar.Brand onClick={() => roar.play()}>
           <img id="mainImg" src={dinosaurImg} alt="dinosaurs" />
         </Navbar.Brand>
       </Navbar.Toggle>
       <Navbar.Collapse id="navbar">
         <Nav fill variant ="tabs" deaultActiveKey='/'className="ms-auto text-center">
-          <Nav.Link className="navbar-copy" href="/">Home</Nav.Link>
-          <Nav.Link className="navbar-copy" href="/about">About</Nav.Link>
-          <Nav.Link className="navbar-copy" href="/shop">Visit</Nav.Link>
-          <Nav.Link className="navbar-copy" href="/checkout">Checkout</Nav.Link>
-          <Nav.Link className="navbar-copy"href="/login">Login</Nav.Link>
-          <Nav.Link className="navbar-copy"href="/" onClick={() => Auth.logout()}>
+          <Nav.Item className="nav-item">
+               <Nav.Link className="navbar-copy" href="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-item">
+               <Nav.Link className="navbar-copy" href="/about">About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-item">
+               <Nav.Link className="navbar-copy" href="/shop">Visit</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-item">
+               <Nav.Link className="navbar-copy" href="/checkout">Checkout</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-item">
+               <Nav.Link className="navbar-copy"href="/login">Login</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-item">
+               <Nav.Link className="navbar-copy"href="/" onClick={() => Auth.logout()}>
             Logout
-          </Nav.Link>
-          <Nav.Link className="navbar-copy" href="/signup">Sign Up</Nav.Link>
-          <Nav.Link className="navbar-copy" href="/contact">Contact</Nav.Link>
+               </Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-item">
+               <Nav.Link className="navbar-copy" href="/signup">Sign Up</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="nav-item">
+            <Nav.Link className="navbar-copy" href="/contact">Contact</Nav.Link>
+          </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
