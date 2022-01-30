70bcf8ff2ea27be52098ff5a7eba240f71fbfc5e
alexdmacon <alexdmacon@gmail.com>
2022-01-29 19:55:41 -0600

    shopping cart with buttons to either check out or log in depending on user auth

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,20 +1,24 @@
 import React from "react";
 import ShoppingCart from "../components/ShoppingCart";
 import { Link } from "react-router-dom";
+import Auth from "../utils/auth"
 // import Cart from "../components/Cart"
 
 const Checkout = () => {
   return (
     <div>
-      <h1>YOU ARE ON THE CHECKOUT PAGE NOW</h1>
-      {/* This will be our payment page with the Stripe boilerplate and form for taking buyer's info.
-    Should include functional "shopping cart" showing what user has purchased so far.*/}
-
       <ShoppingCart /* specific props somewhere in here */ />
-
+      <div className="justify-content-center" align="center">
       <Link to="/shop">
-          <button className="homeBtn">Back to Shop</button>
+          <button className="homeBtn">Continue Shopping for Tour Packages</button>
       </Link>
+      {Auth.loggedIn() ?  ( <Link to="/thanks">
+              <button>Complete Your Purchase</button>
+              </Link> )   :  ( <Link to="/login">
+              <button>You must log in to complete your purchase</button>
+              </Link>
+            )}      
+            </div>
     </div>
     
   );
