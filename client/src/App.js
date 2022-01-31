70a4cae05f96ff9d38a96b56db41ab9e59f63372
Brandyn <hufferbrandyn@gmail.com>
2022-01-31 17:39:28 -0600

    revert aboutpage to about

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,7 @@ import Perks from "./pages/PerkInfo.js";
 import Thanks from "./pages/Thanks";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Linkbar from './components/Linkbar';
-import Jeff from './pages/Jeff';
+import Linkbar from './components/Linkbar'
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -60,23 +59,22 @@ function App() {
           <StoreProvider>
             <Header/>
                  <div className="background mt-5 pt-5">
-                    <main className="h-100 d-flex justify-content-center align-items-center pt-3">
+                    <main className="h-100 d-flex justify-content-center     align-items-center pt-3">
                      <Routes>
                        <Route exact path="/" element={<Home />} />
                        <Route exact path="/shop" element={<Shop />} />
                        <Route exact path="/login" element={<Login />} />
                        <Route exact path="/signup" element={<Signup />} />
-                       <Route exact path="/aboutpage" element={<About />} />
+                       <Route exact path="/about" element={<About />} />
                        <Route exact path="/perks" element={<Perks />} />
                        <Route exact path="/checkout" element={<Checkout />} />
                        <Route exact path="/contact" element={<Contact />} />
                        <Route exact path="/thanks" element={<Thanks />} />
-                       <Route exact path="/jeff" element={<Jeff />} />
                      </Routes>
                     </main>
                  </div>
             <Footer />
-            <Linkbar id="link-container" />
+            <Linkbar id="link-container"/>
             </StoreProvider>
         </>
       </BrowserRouter>
