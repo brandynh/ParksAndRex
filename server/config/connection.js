5a66ce2be4127d34b50ae49ab8b4d7486088fa33
Brandyn <hufferbrandyn@gmail.com>
2022-01-30 21:29:02 -0600

    twweks to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/project3',
+  process.env.MONGODB_URI || 'mongodb://localhost/parksandrex',
   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useMongoClient: true
   }
 );
 
