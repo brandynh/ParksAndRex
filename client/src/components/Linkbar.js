4ba1c8f667664291bef7ae51a9384809b441ac6e
NuminousBlue <jdiehl2236@gmail.com>
2022-01-30 22:24:19 -0600

    initialized linkbar post-footer

---
+++ client/src/components/Linkbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import helpImg from '../assets/images/jurassic-foundation.png';
+import ipsumImg from '../assets/images/dino-ipsum.png';
+
+const Linkbar = () => {
+  return (
+
+   <div id="linkBox" className="container  d-flex justify-content-center mx-auto p-2">
+      <img class="footerImg" id="helpImg" alt="How can you help support dinosaur research? Click here!" src={helpImg} onClick={() => {window.location = "https://www.jurassicfoundation.org/"}} />
+      <img class="footerImg" id="ipsumImg" alt="Do you like dinosaurs?! Click here!" src={ipsumImg} onClick={() => {window.location = "https://dinoipsum.com/"}} />
+   </div>
+   
+   );
+};
+
+export default Linkbar;
